15889cac1289954d4c2c558b44e4c00f9199f819
maxx <maksim.skomorokh@gmail.com>
2020-05-29 00:39:00 +0300

    added and init socket.io

---
+++ server/app.js
@@ -0,0 +1,11 @@
+const app = require("express")()
+const server = require("http").createServer(app)
+const io = require("socket.io")(server)
+
+io.on('connection', ()=>{
+  console.log('IO connection')
+})
+
+module.exports = {
+  app, server
+}
--- server/index.js
+++ server/index.js
@@ -1,17 +1,16 @@
-const express = require('express')
 const consola = require('consola')
-const { Nuxt, Builder } = require('nuxt')
-const app = express()
+const {Nuxt, Builder} = require('nuxt')
+const {app, server} = require("./app")
 
 // Import and Set Nuxt.js options
 const config = require('../nuxt.config.js')
 config.dev = process.env.NODE_ENV !== 'production'
 
-async function start () {
+async function start() {
   // Init Nuxt.js
   const nuxt = new Nuxt(config)
 
-  const { host, port } = nuxt.options.server
+  const {host, port} = nuxt.options.server
 
   await nuxt.ready()
   // Build only in dev mode
@@ -24,10 +23,13 @@ async function start () {
   app.use(nuxt.render)
 
   // Listen the server
-  app.listen(port, host)
-  consola.ready({
-    message: `Server listening on http://${host}:${port}`,
-    badge: true
+  server.listen(port, () => {
+    consola.ready({
+      message: `Server listening on http://${host}:${port}`,
+      badge: true
+    })
   })
+
 }
+
 start()
